b76e2253875e908ba3df6e3c131969a8ed87ec6a
Andrew I Quan <aquan@bu.edu>
2018-12-02 17:15:23 -0500

    makes calls to the pokemon api now

--- habbitTrainer.js
+++ habbitTrainer.js
@@ -1,7 +1,12 @@
-var toDoListTasks = [];
-var dailyTasks = [];
+/*javascript for the habbit trainer 
+ * Author: Andrew Quan
+ * Email: aquan@bu.edu
+*/
 
-var pokemon = [];
+//initailize values 
+var index = 1; //the task id
+var totalCaught = 0; //how many pokemon have been caught
+var uniquePokemon = 0; //how many unique pokemon caught
 
 function addTDLTask() {
 	var input = document.getElementById("tdlTextField").value;
@@ -11,15 +16,63 @@ function addTDLTask() {
     else { //make the task element
     	
     	var newTask = document.createElement("div");
+
+        //make the input checkbox
     	var t = document.createElement("input");
     	t.type = "checkbox";
-    	var textField = document.createTextNode(input);
-    	t.appendChild(textField);
+        t.onclick = "finishTask(taskId)";
+        t.setAttribute("onClick", "finishTask("+index+");" );
+    	var textValue = document.createTextNode(input);
     	newTask.appendChild(t); 
-		newTask.id = "tdlTask";
+        newTask.appendChild(textValue);
+
+        //assign attributes & update id
+		newTask.className = "tdlTask";
+        var taskId = "task" + index;
+        index++;
+        newTask.id = taskId;
+        
 
 		var parent = document.getElementById("ToDoListContent");
     	parent.appendChild(newTask);
-
     }
+}
+
+function finishTask(taskId){
+    /*remove the element */
+    var taskId = "task" + taskId;
+    var removing = document.getElementById(taskId);
+    removing.parentNode.removeChild(removing);
+
+    /*Get a pokemon!*/
+    var randomPokemon = Math.floor(Math.random() * 300) +1;     
+    var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + randomPokemon + "/";
+  
+    $.getJSON(pokeURL, function(data){
+        //console.log(data);
+        //console.log(JSON.stringify(data, null, "  "));
+        //console.log(data["sprites"]["front_default"]);
+
+        //get the name and imgURL from the json
+        var pokemonName = data["name"];
+        var imgURL = data["sprites"]["front_default"];
+
+        //create an image html object and append to the page
+        var image = document.createElement("img");
+        image.src = imgURL;
+        image.alt = "could not load pokemon";
+        image.id = "pokemonImage";
+        var pokemonpopup = document.getElementById("thePicture");
+        pokemonpopup.innerHTML= ''; //clear old image
+        pokemonpopup.appendChild(image);
+
+        //create the caption and append it to the page
+        var captionText ="A wild " + pokemonName + " has appeared!";
+        var caption = document.createTextNode(captionText);
+        var captionDiv = document.getElementById("radarCaption");
+        captionDiv.innerHTML= '';
+        captionDiv.appendChild(caption);
+
+    });
+
 }
