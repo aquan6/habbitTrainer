4e54e4b41da1a2d8e885bb7b9981a53836319dec
Andy - Mac <aquan@bu.edu>
2018-12-02 15:06:28 -0500

    updates

--- habbitTrainer.js
+++ habbitTrainer.js
@@ -1,16 +1,25 @@
 var toDoListTasks = [];
 var dailyTasks = [];
 
+var pokemon = [];
+
 function addTDLTask() {
 	var input = document.getElementById("tdlTextField").value;
     if(input == "") {
     	console.log("No input!"); //e.g. don't do anything
     }
     else { //make the task element
-    	var parent = document.getElementById("ToDoListContent");
+    	
     	var newTask = document.createElement("div");
-    	var t = document.createTextNode("I'm a new Node!");       // Create a text node
-		newTask.appendChild(t); 
+    	var t = document.createElement("input");
+    	t.type = "checkbox";
+    	var textField = document.createTextNode(input);
+    	t.appendChild(textField);
+    	newTask.appendChild(t); 
+		newTask.id = "tdlTask";
+
+		var parent = document.getElementById("ToDoListContent");
     	parent.appendChild(newTask);
+
     }
 }
